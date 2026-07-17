3f8e4e90c4d99771967a41271c3dd70f67ee2d68
Anseltsmm <anseltsm24@gmail.com>
2026-07-17 10:59:55 +0700

    Remove unused AIModel type definition
    Removed AIModel type definition as it was unused.

--- lib/models.ts
+++ lib/models.ts
@@ -1,8 +1,3 @@
-export type AIModel = {
-  id: string
-  name: string
-}
-
 export const MODELS: AIModel[] = [
   { id: "tencent-hy3", name: "Tencent-hy3" },
   { id: "gpt-3.5-turbo", name: "GPT-3.5 Turbo" },
